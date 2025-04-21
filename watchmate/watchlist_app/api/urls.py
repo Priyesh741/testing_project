6c951aaa9952ed9463c9b64d3f4c7bb02749094d
Priyesh <priyeshmishra@acstechconsulting.com>
2025-04-21 07:08:49 +0530

    Some minor change

--- watchmate/watchlist_app/api/urls.py
+++ watchmate/watchlist_app/api/urls.py
@@ -8,8 +8,8 @@ router.register('stream',views.StreamplatformVS,basename="StreamPlatform")
 urlpatterns = [
     path('list/',views.WatchListAV.as_view(),name="movie_list"),
     path('<int:pk>/',views.WatchDetailAV.as_view(),name="movie_details"),
-    # path('platform/',views.StreamPlatformAV.as_view(),name="platform_list"),
-    # path('platform/<int:pk>/',views.StreamDetailAV.as_view(),name="platform_list"),
+    # path('stream/',views.StreamPlatformAV.as_view(),name="platform_list"),
+    # path('stream/<int:pk>/',views.StreamDetailAV.as_view(),name="platform_list"),
 
     path('',include(router.urls)),
 
